b44bf17732bc63eaf324bbe1a575f8c873c0c442
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2023-11-06 21:55:36 -0500

    Feature - Button - ADDED basic functionality

---
+++ dev/script.js
@@ -0,0 +1,38 @@
+var ingredientPage = document.querySelector(".ingredientPage")
+var recipePage = document.querySelector(".recipePage")
+var startPage = document.querySelector(".startPage")
+var startBtn = document.querySelector("#startBtn")
+var ingredientBtn = document.querySelector("#ingredientBtn")
+var regenerateBtn = document.querySelector("#regenerateBtn")
+var restartBtn = document.querySelector("#restartBtn")
+
+// Function to hide the selected HTML element and its nest HTML elements by adding a "inactive" CSS class
+function hide(element){
+    element.classList.add("inactive");
+}
+
+// Function to show the selected HTML element and its nested HTML elements by removing the "inactive" CSS class
+function show(element){
+    element.classList.remove('inactive');
+}
+
+window.addEventListener('load', function() {
+    hide(ingredientPage);
+    hide(recipePage);
+});
+
+startBtn.addEventListener('click', function(){
+    hide(startPage)
+    show(ingredientPage)
+})
+
+ingredientBtn.addEventListener('click', function(){
+    hide(ingredientPage)
+    show(recipePage)
+})
+
+restartBtn.addEventListener('click', function(){
+    hide(ingredientPage);
+    hide(recipePage);
+    show(startPage)
+})
